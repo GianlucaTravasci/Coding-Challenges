a9a772c741e2436976645f1fbdbd88334be81f6c
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-12-04 10:07:18 +0100

    solution two

--- advent-of-code/2020/Day 4/solution4.py
+++ advent-of-code/2020/Day 4/solution4.py
@@ -33,7 +33,25 @@ def compute_first(passports):
         p = passports[i]
         if 'byr' in p and 'iyr' in p and 'eyr' in p and 'hgt' in p and 'hcl' in p and 'ecl' in p and 'pid' in p:
             valid_counter_first += 1
-            
+            hcl_re = re.compile('^#[a-f0-9]{6}$')
+            pid_re = re.compile('^[0-9]{9}$')
+            if (
+                1920 <= int(p['byr']) <= 2002 and
+                2010 <= int(p['iyr']) <= 2020 and
+                2020 <= int(p['eyr']) <= 2030 and
+                (
+                 (p['hgt'][-2:] == 'in' and
+                  59 <= int(p['hgt'][0:-2]) <= 76
+                 ) or
+                 (p['hgt'][-2:] == 'cm' and
+                  150 <= int(p['hgt'][0:-2]) <= 193
+                 )
+                ) and
+                hcl_re.match(p['hcl']) and
+                p['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'] and
+                pid_re.match(p['pid'])
+            ):
+                valid_counter_second += 1
     return valid_counter_first, valid_counter_second
 
 
