89793d7601d32ce6f1702eec47ceff941bca7d95
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-12-05 10:34:14 +0100

    place decoder from given input

--- advent-of-code/2020/Day 5/solution5.py
+++ advent-of-code/2020/Day 5/solution5.py
@@ -4,4 +4,17 @@ def get_input_from_file():
         return seats
 
 
+def decode_place(code, row_col):
+    for char in code:
+        if char == 'F' or char == 'L':
+            row_col[0] = row_col[0]
+            row_col[1] = (row_col[0] + row_col[1]) // 2
+        elif char == 'B' or char == 'R':
+            row_col[0] = (row_col[0] + row_col[1]) // 2 + 1
+            row_col[1] = row_col[1]
+    if code[-1] == 'F' or code[-1] == 'L':
+        return row_col[0]
+    else:
+        return row_col[1]
+
 
