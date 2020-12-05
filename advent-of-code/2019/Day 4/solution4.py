a349486cbd143d43e05c88f178220c9f08964248
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-12-05 21:52:53 +0100

    day 4 for 2019 done

--- advent-of-code/2019/Day 4/solution4.py
+++ advent-of-code/2019/Day 4/solution4.py
@@ -1,25 +1,44 @@
-def valid_password(x):
-    for i in range(len(x) - 1):
-        if x[i] > x[i + 1]:
+def valid_password(digit):
+    is_valid = False
+    last_digit = '0'
+    for i in range(len(digit)):
+        if digit[i] < last_digit:
             return False
+        if digit[i] == last_digit:
+            is_valid = True
+        last_digit = digit[i]
+    return is_valid
 
-    for i in range(len(x) - 1):
-        for i in range(len(x) - 1):
-            if x[i] == x[i + 1]:
-                if (i > 0 and x[i - 1] == x[i]) or (i < len(x) - 2 and x[i + 2] == x[i + 1]):
-                    continue
-                else:
-                    return True
+
+def valid_password_two(digit):
+    is_valid = False
+    digit_group_started = 1
+    last_digit = '0'
+    for i in range(len(digit)):
+        if digit[i] < last_digit:
+            return False
+        if digit[i] == last_digit:
+            digit_group_started += 1
+        else:
+            if digit_group_started == 2:
+                is_valid = True
+            digit_group_started = 1
+        last_digit = digit[i]
+    if digit_group_started == 2:
+        is_valid = True
+    return is_valid
 
 
 if __name__ == '__main__':
-
     with open('input.txt') as f:
         low, high = [int(x) for x in f.read().split('-')]
-
     c = 0
+    d = 0
     for i in range(low, high + 1):
         if valid_password(str(i)):
             c += 1
+        if valid_password_two(str(i)):
+            d += 1
 
-    print(c)
+    print('solution 1: ', c)
+    print('solution 2: ', d)
