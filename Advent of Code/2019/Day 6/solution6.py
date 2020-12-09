1549ec87738aad0b7e7bc0def889c0b2e2caf71a
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-12-09 20:59:15 +0100

    part one done

--- Advent of Code/2019/Day 6/solution6.py
+++ Advent of Code/2019/Day 6/solution6.py
@@ -32,6 +32,26 @@ def input_reader():
 input_reader()
 
 
+def part1():
+    total_orbits = 0
+    # The list of planets yet to be processed
+    # This list will be modified as we process the planets
+    remaining = ['COM']
+    while len(remaining) > 0:
+        planet = remaining[0]
+        orbit_count = orbits[planet]['count']
+        # By looping over each planet's children, we are sure to visit every planet
+        # once and only once because each planet is the child of exactly one parent planet
+        for c in orbits[planet]['children']:
+            orbits[c]['count'] = orbit_count + 1
+            total_orbits += orbit_count + 1
+        # Add all the children planets of the current planet to our list
+        # And remove the current planet from it
+        remaining.extend(orbits[planet]['children'])
+        remaining = remaining[1:]
+    return total_orbits
+
+
 
 
 print(part1())
