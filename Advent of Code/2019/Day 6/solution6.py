8e7dbd330d815c31e7be6e64549919e07d11bad9
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-12-09 20:59:36 +0100

    part 2 done

--- Advent of Code/2019/Day 6/solution6.py
+++ Advent of Code/2019/Day 6/solution6.py
@@ -52,6 +52,28 @@ def part1():
     return total_orbits
 
 
+def part2():
+    # Create two lists that traces all the parents of each planet back to COM
+    # Then find the first planet in common between the two and count the orbital transfers
+    # to that common node using the indices of the lists
+    you = 'YOU'
+    san = 'SAN'
+    you_trace = []
+    san_trace = []
+
+    next_parent = orbits[you]['parent']
+    while next_parent:
+        you_trace.append(next_parent)
+        next_parent = orbits[next_parent]['parent']
+
+    next_parent = orbits[san]['parent']
+    while next_parent:
+        san_trace.append(next_parent)
+        next_parent = orbits[next_parent]['parent']
+
+    for i, c in enumerate(you_trace):
+        if c in san_trace:
+            return i + san_trace.index(c)
 
 
 print(part1())
