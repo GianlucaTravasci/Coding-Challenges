c411db541844c2188c72e3b19f2fc146911bd323
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-12-11 10:34:06 +0100

    valid seat checker

--- Advent of Code/2020/Day 11/solution11.py
+++ Advent of Code/2020/Day 11/solution11.py
@@ -10,6 +10,12 @@ row = len(seats)
 col = len(seats[0])
 
 
+def check_valid_seat(i, j):
+    if i < 0 or j < 0 or i >= row or j >= col:
+        return False
+    return True
+
+
 
 
 solve(seats, 1) # Part 1
