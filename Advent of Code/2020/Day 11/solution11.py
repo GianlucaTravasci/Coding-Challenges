a7c6a768bdb1461572d10e3295b7a8d4ff06270f
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-12-11 10:34:51 +0100

    check the adjacent seats

--- Advent of Code/2020/Day 11/solution11.py
+++ Advent of Code/2020/Day 11/solution11.py
@@ -16,6 +16,27 @@ def check_valid_seat(i, j):
     return True
 
 
+def check_valid_seat(i, j):
+    if i < 0 or j < 0 or i >= row or j >= col:
+        return False
+    return True
+
+
+def check_adjacent_seats(i, j, grid, part):
+    x = [-1, 0, 1, -1, 1, -1, 0, 1]
+    y = [1, 1, 1, 0, 0, -1, -1, -1]
+    occupied = 0
+    for k in range(8):
+        adj_i = i + x[k]
+        adj_j = j + y[k]
+        while check_valid_seat(adj_i, adj_j) and grid[adj_i][adj_j] == '.' and part == 2:
+            adj_i += x[k]
+            adj_j += y[k]
+        if check_valid_seat(adj_i, adj_j) and grid[adj_i][adj_j] == '#':
+            occupied += 1
+    return occupied
+
+
 
 
 solve(seats, 1) # Part 1
