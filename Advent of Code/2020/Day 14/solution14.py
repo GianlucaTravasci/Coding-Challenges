ce1ee667ff045fc793564d113fc82d1047f6f6ba
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-12-14 15:36:21 +0100

    add the func that calculate the mask value

--- Advent of Code/2020/Day 14/solution14.py
+++ Advent of Code/2020/Day 14/solution14.py
@@ -1,4 +1,5 @@
 import re
+from itertools import zip_longest, product
 
 
 def parse_input(line):
@@ -9,9 +10,17 @@ def parse_input(line):
         return int(re.findall(r'\d+', key)[0]), bin(int(val))[2:]
 
 
+def mask_value(mask, val):
+    return int(
+        ''.join([digit if m == 'X' else m for m, digit in zip_longest(mask[::-1], val[::-1], fillvalue='0')][::-1]), 2)
+
+
+
+
+
 if __name__ == "__main__":
     with open("input.txt") as file:
         record = [parse_input(line) for line in file.read().splitlines()]
 
-    print("Part one: ", record)
+    print("Part one: ", part1(record))
     print("Part two: ")
