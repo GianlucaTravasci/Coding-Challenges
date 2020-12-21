ff7ba4e2efc2452c2df0e4ee4228bf4061f86a04
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-12-21 10:55:15 +0100

    operator finder

--- Advent of Code/2020/Day 18/solution18.py
+++ Advent of Code/2020/Day 18/solution18.py
@@ -5,3 +5,21 @@ with open('input.txt') as file:
     expressions = [line.replace(' ', '') for line in file.read().splitlines()]
 
 
+def get_operator(expression, advanced):
+    depth = 0
+    candidate = None
+    for i, symbol in enumerate(reversed(expression)):
+        if symbol == '(':
+            depth += 1
+        if symbol == ')':
+            depth -= 1
+        if symbol == '*' and depth == 0:
+            candidate = mul, len(expression) - i - 1
+            break
+        if symbol == '+' and depth == 0:
+            candidate = add, len(expression) - i - 1
+            if not advanced:
+                break
+    return candidate
+
+
